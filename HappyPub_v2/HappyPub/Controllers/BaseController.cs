0539b00e8dc9a34450ad1ff193ef43a80cf06a27
venustingleung <ting.leung-1@students.plymouth.ac.uk>
2020-02-16 05:18:36 +0800

    new update-have database, interface

---
+++ HappyPub_v2/HappyPub/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HappyPub.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        public BaseController()
+        {
+
+        }
+    }
+}
---
+++ HappyPub_v2/HappyPub/DAL/PubContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HappyPub.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+namespace HappyPub.DAL
+{
+    public class PubContext : DbContext
+    {
+        public PubContext() : base("PubContext")
+        {
+        }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
