0cb82e85b83c92333115300b8e1e63d9e4372cdc
venustingleung <ting.leung-1@students.plymouth.ac.uk>
2020-02-16 03:00:11 +0800

    New update with role, datebase-fail

---
+++ HappyPub/~AutoRecover.Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace HappyPub.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public decimal Total { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        public virtual List<OrderItem> OrderItem { get; set; }
+    }
+}
